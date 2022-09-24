246e22f734fb9a7f2dd92c2f8263f671b4d877f8
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-09-24 14:53:52 -0400

    NEW: functional seeking

--- src/components/videoplayer/index.tsx
+++ src/components/videoplayer/index.tsx
@@ -1,96 +1,97 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Video from 'react-native-video';
+
+import { handleMuteUnmute, handlePlayPause, onLoad, onProgress } from './events';
 import PlayerControls from './playercontrols';
-import {
-  handleMuteUnmute,
-  handlePlayPause,
-  onLoad,
-  onProgress,
-} from './events';
 
 const VideoPlayer = () => {
-  const videoRef = React.useRef<any>(null);
-  let seekerWidth = 0;
-  const [state, setState] = useState<any>({
-    fullscreen: false,
-    currentTime: 0,
-    duration: 0,
-    muted: false,
-    showControls: true,
-    seeking: false,
-    rate: 1,
-    volume: 1,
-    resizeMode: 'contain',
-    videoWidth: 0,
-    videoHeight: 0,
-    paused: false,
-    decoration: true,
-    isLoading: false,
-    stable: false,
-    seekerFillWidth: 0,
-    seekerPosition: 0,
-    seekerOffset: 0,
-    audioTracks: [],
-    textTracks: [],
-    selectedAudioTrack: undefined,
-    selectedTextTrack: undefined,
-    srcListId: 0,
-    loop: false,
-  });
+    const videoRef = React.useRef<any>(null);
+    let seekerWidth = 0;
+    const [state, setState] = useState<any>({
+        fullscreen: false,
+        currentTime: 0,
+        duration: 0,
+        muted: false,
+        showControls: true,
+        seeking: false,
+        rate: 1,
+        volume: 1,
+        resizeMode: 'contain',
+        videoWidth: 0,
+        videoHeight: 0,
+        paused: false,
+        decoration: true,
+        isLoading: false,
+        stable: false,
+        seekerFillWidth: 0,
+        seekerPosition: 0,
+        seekerOffset: 0,
+        audioTracks: [],
+        textTracks: [],
+        selectedAudioTrack: undefined,
+        selectedTextTrack: undefined,
+        srcListId: 0,
+        loop: false,
+    });
 
-  const onSeek = (seek: any) => {
-    //Handler for change in seekbar
-    videoRef.current.seek(seek);
-  };
+    const onForceSeek = (seek: any) => {
+        //Handler for change in seekbar
+        videoRef.current.seek(seek);
+    };
 
-  const onSeeking = (currentTime: any) => setState({ ...state, currentTime });
+    const onSeeking = (currentTime: any) => setState({ ...state, currentTime });
 
-  return (
-    <View
-      style={styles.container}
-      onLayout={event => (seekerWidth = event.nativeEvent.layout.width)}>
-      <Video
-        ref={videoRef}
-        style={styles.video}
-        controls={false}
-        paused={!state.paused}
-        muted={state.muted}
-        // onProgress={() =>
-        //   onProgress({ data: videoRef.current, state, setState })
-        // }
-        onLoad={data => onLoad({ data, setState })}
-        onProgress={data => onProgress({ data, setState })}
-        resizeMode={'contain'}
-        source={{
-          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-        }}
-      />
-      <PlayerControls
-        state={state}
-        setState={setState}
-        playing={state.paused}
-        muted={state.muted}
-        currentTime={state.currentTime}
-        duration={state.duration}
-        onSlidingStart={() => handlePlayPause({ state, setState })}
-        onSlidingComplete={() => handlePlayPause({ state, setState })}
-        onPlay={() => handlePlayPause({ state, setState })}
-        onPause={() => handlePlayPause({ state, setState })}
-        onMute={() => handleMuteUnmute({ state, setState })}
-        onUnmute={() => handleMuteUnmute({ state, setState })}
-      />
-    </View>
-  );
+    return (
+        <View
+            style={styles.container}
+            onLayout={(event) => (seekerWidth = event.nativeEvent.layout.width)}
+        >
+            <Video
+                ref={videoRef}
+                style={styles.video}
+                controls={false}
+                paused={!state.paused}
+                muted={state.muted}
+                // onProgress={() =>
+                //   onProgress({ data: videoRef.current, state, setState })
+                // }
+                onLoad={(data) => onLoad({ data, setState })}
+                onProgress={(data) => onProgress({ data, setState })}
+                resizeMode={'contain'}
+                source={{
+                    uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+                }}
+            />
+            <PlayerControls
+                state={state}
+                setState={setState}
+                playing={state.paused}
+                muted={state.muted}
+                currentTime={state.currentTime}
+                duration={state.duration}
+                onSlidingStart={() => setState({ ...state, seeking: true })}
+                onSlidingComplete={(data: number) => {
+                    videoRef.current.seek(data);
+                    setState({ ...state, seeking: false, currentTime: data });
+                }}
+                onForceSeek={onForceSeek}
+                onPlay={() => handlePlayPause({ state, setState })}
+                onPause={() => handlePlayPause({ state, setState })}
+                onMute={() => handleMuteUnmute({ state, setState })}
+                onUnmute={() => handleMuteUnmute({ state, setState })}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'relative',
-  },
-  video: {
-    aspectRatio: 16 / 9,
-  },
+    container: {
+        position: 'relative',
+    },
+    video: {
+        aspectRatio: 16 / 9,
+    },
 });
 
 export default VideoPlayer;
--- src/components/videoplayer/playercontrols.tsx
+++ src/components/videoplayer/playercontrols.tsx
@@ -1,128 +1,129 @@
+import Slider from '@react-native-community/slider';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { IconButton, Text } from 'react-native-paper';
-import Slider from '@react-native-community/slider';
+
 import AdditionalControls from './additionalcontrols';
-import { secondsToHms } from './utils';
 import { handlePlayPause, onSeek } from './events';
+import { secondsToHms } from './utils';
 
 const PlayerControls = ({
-  videoRef,
-  state,
-  setState,
-  playing,
-  muted,
-  currentTime,
-  duration,
-  showPreviousAndNext,
-  showSkip,
-  previousDisabled,
-  nextDisabled,
-  onSlidingStart,
-  onSlidingComplete,
-  onPlay,
-  onPause,
-  onMute,
-  onUnmute,
-  skipForwards,
-  skipBackwards,
-  onNext,
-  onPrevious,
+    videoRef,
+    state,
+    setState,
+    playing,
+    muted,
+    currentTime,
+    duration,
+    showPreviousAndNext,
+    showSkip,
+    previousDisabled,
+    nextDisabled,
+    onSlidingStart,
+    onSlidingComplete,
+    onPlay,
+    onPause,
+    onMute,
+    onUnmute,
+    onForceSeek,
+    skipForwards,
+    skipBackwards,
+    onNext,
+    onPrevious,
 }: any) => {
-  const [visible, setVisible] = React.useState(false);
-  const showDialog = () => setVisible(true);
-  return (
-    <View style={styles.container}>
-      <View style={styles.controlsTop}>
-        <IconButton
-          size={25}
-          iconColor="#ffffff"
-          onPress={muted ? onUnmute : onMute}
-          icon={muted ? 'volume-off' : 'volume-high'}
-        />
-        <View style={styles.topLeft}>
-          <IconButton
-            size={25}
-            iconColor="#ffffff"
-            onPress={showDialog}
-            icon="dots-vertical"
-          />
-          <AdditionalControls visible={visible} setVisible={setVisible} />
-          <IconButton
-            size={25}
-            iconColor="#ffffff"
-            onPress={() => console.log('Pressed')}
-            icon="fullscreen"
-          />
+    const [visible, setVisible] = React.useState(false);
+    const showDialog = () => setVisible(true);
+    return (
+        <View style={styles.container}>
+            <View style={styles.controlsTop}>
+                <IconButton
+                    size={25}
+                    iconColor='#ffffff'
+                    onPress={muted ? onUnmute : onMute}
+                    icon={muted ? 'volume-off' : 'volume-high'}
+                />
+                <View style={styles.topLeft}>
+                    <IconButton
+                        size={25}
+                        iconColor='#ffffff'
+                        onPress={showDialog}
+                        icon='dots-vertical'
+                    />
+                    <AdditionalControls visible={visible} setVisible={setVisible} />
+                    <IconButton
+                        size={25}
+                        iconColor='#ffffff'
+                        onPress={() => console.log('Pressed')}
+                        icon='fullscreen'
+                    />
+                </View>
+            </View>
+            <View style={styles.controlsCenter}>
+                <IconButton
+                    size={50}
+                    iconColor='#ffffff'
+                    onPress={playing ? onPause : onPlay}
+                    icon={playing ? 'pause' : 'play'}
+                />
+            </View>
+            <View style={styles.controlsBottom}>
+                <View
+                    style={{
+                        justifyContent: 'space-between',
+                        flexDirection: 'row',
+                        width: '100%',
+                        paddingLeft: 20,
+                        paddingRight: 20,
+                        paddingBottom: 10,
+                    }}
+                >
+                    <Text>{secondsToHms(currentTime || 1)}</Text>
+                    <Text>{secondsToHms(duration)}</Text>
+                </View>
+                {state.stable && (
+                    <Slider
+                        value={currentTime}
+                        minimumValue={0}
+                        maximumValue={duration}
+                        step={1}
+                        onSlidingStart={onSlidingStart}
+                        onSlidingComplete={onSlidingComplete}
+                        onValueChange={(data) => onSeek({ data, state, setState, videoRef })}
+                        minimumTrackTintColor={'#F44336'}
+                        maximumTrackTintColor={'#FFFFFF'}
+                        thumbTintColor={'#F44336'}
+                    />
+                )}
+            </View>
         </View>
-      </View>
-      <View style={styles.controlsCenter}>
-        <IconButton
-          size={50}
-          iconColor="#ffffff"
-          onPress={playing ? onPause : onPlay}
-          icon={playing ? 'pause' : 'play'}
-        />
-      </View>
-      <View style={styles.controlsBottom}>
-        <View
-          style={{
-            justifyContent: 'space-between',
-            flexDirection: 'row',
-            width: '100%',
-            paddingLeft: 20,
-            paddingRight: 20,
-            paddingBottom: 10,
-          }}>
-          <Text>{secondsToHms(currentTime || 1)}</Text>
-          <Text>{secondsToHms(duration)}</Text>
-        </View>
-        {state.stable && (
-          <Slider
-            value={currentTime}
-            minimumValue={0}
-            maximumValue={duration}
-            step={1}
-            onSlidingStart={onSlidingStart}
-            onSlidingComplete={onSlidingComplete}
-            onValueChange={data =>
-              onSeek({ state, setState, videoRef, seekTime: data })
-            }
-            minimumTrackTintColor={'#F44336'}
-            maximumTrackTintColor={'#FFFFFF'}
-            thumbTintColor={'#F44336'}
-          />
-        )}
-      </View>
-    </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000b3',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    top: 0,
-    left: 0,
-  },
-  controlsTop: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
-  },
-  topLeft: {
-    flexDirection: 'row',
-  },
-  controlsCenter: {},
-  controlsBottom: {
-    width: '100%',
-    marginBottom: 20,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#000000b3',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+    },
+    controlsTop: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+    },
+    topLeft: {
+        flexDirection: 'row',
+    },
+    controlsCenter: {},
+    controlsBottom: {
+        width: '100%',
+        marginBottom: 20,
+    },
 });
 
 export default PlayerControls;
